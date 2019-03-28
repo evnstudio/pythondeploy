b9318733f7532003d0458bfab51ee9b99db566e6
Ibrahimi <44783560+envstudio@users.noreply.github.com>
2019-03-28 21:22:00 +0800

    Create pydeploy.py

---
+++ pydeploy.py
@@ -0,0 +1,31 @@
+#/usr/bin/python3
+
+import sys, paramiko,os
+import subprocess
+
+datacommit =sys.argv[1]
+ssh = paramiko.SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.WarningPolicy)
+host=<host>
+username=<username>
+password=<password>
+dirFolder=<directory>
+
+def pull():
+    ssh.connect(hostname=host, username=username, password=password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("screen -dmS killnode killall node && cd + dirFolder + && git pull && screen -dmS node npm run dev")
+    print(ssh_stdout.read())
+    ssh.close()
+
+def main():
+    try:
+        subprocess.check_call(["git add ."], shell=True)
+        subprocess.check_call("git commit -m update", shell=True)
+        subprocess.check_call("git push origin "+datacommit, shell=True)
+        pull()
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("nothing change")
+
+if __name__ == '__main__':
+    main()
